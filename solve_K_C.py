1489b4d427d61cd56e3326a0f24483256a887700
TuubaLord <osma.ovaskainen@gmail.com>
2025-11-24 10:36:02 +0200

    add end to end implementation

---
+++ solve_K_C.py
@@ -0,0 +1,12 @@
+from compute_s import compute_s
+from compute_matrices import compute_K, compute_C
+from eccentricity import solve_eccentricity
+"""
+Uses solvers for s, eccentricity, and compute K and compute C functions to provide end to end solution for K and C from initial values
+"""
+def solve_K_C(D, omega, eta, L, f, c):
+    S = compute_s(D, omega, eta, L, f, c)
+    eps = solve_eccentricity(S)
+    K = compute_K(c, eps)
+    C = compute_C(c, omega, eps)
+    return K, C
